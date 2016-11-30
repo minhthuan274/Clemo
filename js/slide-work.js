f5d4a59dca1eda333c99687923500355be0df4b1
Minh Thuan <doanminhthuan274@gmail.com>
2016-11-30 20:39:45 +0700

    changed event on service

--- js/slide-work.js
+++ js/slide-work.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {
-
+	$('#indicators-slide-work li, #control-slide-work a').click(function(event) {
 		var numberSlide = $('#number-slide');
 		var numberCurSlide =$('#indicators-slide-work .active').attr('number');
 		numberSlide.text(numberCurSlide +'/5');
+	});
 });
